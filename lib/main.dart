b760d2c888cbd8a96d995bd8953749c77ce74000
Haitham <haithampro02@gmail.com>
2023-11-30 21:36:58 +0300

    Hello GiTHUB

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'saif bataineh'),
+      home: const MyHomePage(title: 'خليها على الله'),
     );
   }
 }
