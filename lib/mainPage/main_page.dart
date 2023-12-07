90ea94df890f263f892c002c980e4a8c583d604d
saifbataineh <saifbbataineh@gmail.com>
2023-12-07 17:41:30 +0300

    finallt

--- lib/mainPage/main_page.dart
+++ lib/mainPage/main_page.dart
@@ -1,11 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:grad_test_1/mainPage/auto_complete.dart/autocmp_class.dart';
 import 'package:grad_test_1/mainPage/pop_restric.dart';
+import 'package:grad_test_1/sign-in-up-page/welcome_page.dart';
 
 class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PopRestrict(),
+      appBar: AppBar(
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 0.5),
+              child: IconButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (ctx)=>const WelcomePage()), (route) => false);
+                  },
+                  icon: const Icon(Icons.logout)))
+        ],
+      ),
+      body: const Column(children: [
+        AutoCompleteSearch(),
+
+        PopRestrict(),
+      ],
+        
+        
+      ),
     );
   }
 }
