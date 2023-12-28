2b3f9c64e8105d8ac3519c819e780d927917dbef
saifbataineh <saifbbataineh@gmail.com>
2023-12-28 17:04:59 +0300

    auto complete is almost done

--- lib/Providers/listen_provider.dart
+++ lib/Providers/listen_provider.dart
@@ -1,9 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TextProvider extends ChangeNotifier {
   String _text = "";
 
+  Map<String, dynamic> _map = {};
+
   String get text => _text;
+
+  Future<void> get fetchData async {
+    final String response =
+        await rootBundle.loadString('assets/json/datapharma.json') ;
+
+    _map = await json.decode(response);
+    
+    notifyListeners();
+  }
+
+  void initialValues() {
+    _map = {};
+    notifyListeners();
+  }
+
+  Map<String, dynamic> get map => _map;
 
   void setText(String text) {
     _text = text;
